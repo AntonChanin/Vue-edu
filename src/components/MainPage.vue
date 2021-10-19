cc598808eb94450f72ea8d68d4db2dd445b0a131
antonvh <antonhaninvl@gmail.com>
2021-10-19 11:41:12 +0300

    main page update

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -12,26 +12,59 @@
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
     </div>
+    <button class="button-data-base" v-on:click="isShow = !isShow">
+      FIND POST IN DB
+    </button>
+    <div v-if="isShow" class="find-form">
+      <MyCard>
+        <template #content>
+          <div v-if="findPost.id">
+            <span>Post Id</span><input placeholder="Found Id" />
+            <button class="button-data-base" v-on:click="featchPostsById(3)">
+              START FIND
+            </button>
+          </div>
+          <div v-else>
+            <span>{{ findPost }}</span>
+            <button
+              class="button-data-base"
+              v-on:click="
+                isShow = false;
+                findPost = {};
+              "
+            >
+              END
+            </button>
+          </div>
+        </template>
+      </MyCard>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from "vuex";
+import { mapActions, mapGetters, mapState } from "vuex";
+import MyCard from "../components/MyCard.vue";
 
 export default {
   name: "MainPage",
   props: {},
+  components: {
+    MyCard,
+  },
   data: function () {
     return {
       title: `Main page`,
       lorem: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+      isShow: false,
     };
   },
   computed: {
-    ...mapState("posts", ["posts"]),
+    ...mapState("posts", ["posts", "findPost"]),
+    ...mapGetters("posts", ["getShowForm"]),
   },
   methods: {
-    ...mapActions("posts", ["featchPosts"]),
+    ...mapActions("posts", ["featchPosts", "featchPostsById"]),
   },
 
   async mounted() {
@@ -44,12 +77,26 @@ export default {
 h1 {
   margin: 24px auto;
 }
+.button-data-base {
+  background-color: #000;
+  border: none;
+  color: #fff;
+  font-size: 18px;
+  outline: none;
+  height: 40px;
+}
+.button-data-base:active {
+  background-color: #292929;
+}
 .col {
   display: flex;
   flex-direction: column;
   max-width: 820px;
   margin: auto;
   padding: 0 10px;
+}
+.find-form {
+  position: absolute;
 }
 
 .row {
--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -1,11 +1,16 @@
 const posts = {
   namespaced: true,
   state: {
-    posts: []
+    posts: [],
+    findPost: {},
+    isShow: false
   },
   mutations: {
     UPDATE_POSTS: (state, payload) => {
       state.posts = payload;
+    },
+    UPDATE_POST: (state, payload) => {
+      state.findPost = payload;
     }
   },
   actions: {
@@ -13,6 +18,18 @@ const posts = {
       const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=3');
       const posts = await res.json();
       ctx.commit('UPDATE_POSTS', posts);
+    },
+    featchPostsById: async (ctx, id) => {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?id=${id}`);
+      const post = await res.json();
+      ctx.commit('UPDATE_POST', post);
+      console.log(post);
+    }
+  },
+  getters: {
+    getShowForm: async (state) => {
+      console.log('state.isShow', state.isShow)
+      state.isShow = !state.isShow;
     }
   }
 }
